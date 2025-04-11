7a39a7838b444e7dde7fb04f5f94766dec6bf6b0
Alex P <alexperis95@gmail.com>
2025-04-11 20:51:39 +0200

    feat: :sparkles: Catalog done

--- src/app/pages/catalog/catalog.component.ts
+++ src/app/pages/catalog/catalog.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { EventsService } from '../../services/events.service';
+import { Concert } from '../../models/interfaces';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-catalog',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.scss'
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit{
+  
+  private eventService = inject(EventsService);
+  private router = inject(Router);
+  private sanitizer = inject(DomSanitizer);
 
+  events: Concert[] = [];
+
+  ngOnInit(): void {
+    this.eventService.getEvents().subscribe(data => {
+      this.events = data.map(event => ({
+        ...event,
+        safeDescription: this.sanitizer.bypassSecurityTrustHtml(event.description)
+      }))
+      .sort((a: any, b:any) => a.endDate - b.endDate);  
+    });
+  }
+  
+
+  navigateToEvent(id:string){
+    this.router.navigateByUrl(`/event/${id}`);
+  }
 }
